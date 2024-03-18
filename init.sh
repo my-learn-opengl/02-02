51ce9f97ba0dbaf744eefcc8e364f734d78028de
Juyeong Maing <mjy9088@naver.com>
2024-03-19 01:38:21 +0900

    initial commit

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# glfw
+cmake -B builddir/glfw -DCMAKE_INSTALL_PREFIX:PATH=../../external submodules/glfw
+cmake --build builddir/glfw --target install --config Release --parallel
+
+# glfw
+python3 -m venv venv
+. venv/bin/activate
+(cd submodules/glad && pip3 install -r requirements.txt && python3 -m glad --out-path=../../external --api=gl:core=3.3 c)
+deactivate
